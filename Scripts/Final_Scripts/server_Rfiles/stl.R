e396a9509e75c513e17e9d1d94d4bd1c7fb76f45
Gopalakrishna <T.Gopalakrishna@exeter.ac.uk>
2025-03-06 21:37:40 +0000

    Moved new stl. Rmd to R script to server folder to run on server

--- Scripts/Final_Scripts/server_Rfiles/stl.R
+++ Scripts/Final_Scripts/server_Rfiles/stl.R
@@ -13,49 +13,54 @@ doParallel::registerDoParallel(cl = my.cluster)
 foreach::getDoParRegistered() #should be TRUE
 foreach::getDoParWorkers() #should give numCores
 
-Sys.time()
-file_list <- list.files(path = "/dungbeetle/home/tg505/Trisha/ecothresholds/Outputs/Indices/NDVI/Max_consecutive_missingNA/meanfill_southern_dfs/", 
-                        pattern='.rds$', all.files=TRUE, full.names=TRUE)
-rds_list<-lapply(file_list, read_rds)
-Sys.time()
-length(rds_list)
+index_file_path <- "/dungbeetle/home/tg505/Trisha/ecothresholds/Outputs/Indices/"
 
-Sys.time(); print ("Unique number of cells check")
-unique_cell_df<- data.frame(matrix(ncol = 2, nrow = 0))
-x <- c("df_number", "number_uniquecells")
-colnames(unique_cell_df) <- x
-for (i in 2001:3000){
-  unique_cell_df[i,1]<-i
-  unique_cells<-length(unique(rds_list[[i]]$cell))
-  unique_cell_df[i,2]<- unique_cells
-}
-
-write.csv(unique_cell_df, "/dungbeetle/home/tg505/Trisha/ecothresholds/Outputs/Indices/NDVI/STL_Decomposition/swindow_11/unique_cell_nrowcheck.csv")
-
-Sys.time(); print ("STL start")
-stl<- function (split_pivot_df){
-  stl_decomposition<- function (df, swin){
-    df_ts<- ts(df$value_int, start=c(2002,1), end=c(2021,12), frequency=12)
-    stl_df<- stlplus::stlplus(df_ts, s.window = swin, s.degree = 1, t.degree = 1)
-    df_needed<- (stl_df$data)
-    df_needed<- df_needed %>% dplyr::select(-c(weights, sub.labels ))
-    df_needed
-  }
-  message ("Stl function defined")
+stl_function <- function (index_name, input_rds_climate_zone_folder, swin, swinfolder, climate_zone){
+  file_list<- list.files (path = paste0(index_file_path, index_name,"/", "Max_consecutive_missingNA/", input_rds_climate_zone_folder, "/"), pattern =".rds$", all.files = T, full.names = T)
+  file_list <- gtools::mixedsort(file_list)
+  x<- split(file_list, ceiling(seq_along(file_list)/1000))
   
-  results <- foreach(
-    i= 2001:3000,
-    .combine = "rbind") %dopar% {
-      split_pivot_df[[i]] %>%
-        group_by(cell) %>%
-        nest() %>%
-        mutate(stl_data = purrr::map(data, stl_decomposition, swin=11)) %>%
-        unnest(c(data,stl_data)) %>%
-        dplyr::select(-c(Year, Month, raw))
+  for (i in 1:length(x)){
+    rds_list<- lapply(x[[i]], read_rds)
+    stl_decomposition<- function (df, swin){
+      df_ts<- ts(df$value_int, start=c(2002,1), end=c(2021,12), frequency=12)
+      stl_df<- stlplus::stlplus(df_ts, s.window = swin, s.degree = 1, t.degree = 1)
+      df_needed<- (stl_df$data)
+      df_needed<- df_needed %>% dplyr::select(-c(weights, sub.labels ))
+      df_needed
     }
-  message("Stl decomposition complete")
-  write_rds(results, "/dungbeetle/home/tg505/Trisha/ecothresholds/Outputs/Indices/NDVI/STL_Decomposition/swindow_11/stl_southern2001_3000.rds")
-  message ("rds written out")
+    message ("Stl function defined")
+    
+    results <- foreach(
+      i= 1:length(rds_list),
+      .combine = "rbind") %dopar% {
+        rds_list[[i]] %>%
+          group_by(cell) %>%
+          nest() %>%
+          mutate(stl_data = purrr::map(data, stl_decomposition, swin= swin)) %>%
+          unnest(c(data,stl_data)) %>%
+          dplyr::select(-c(Year, Month, raw))
+      }
+    message("Stl decomposition complete")
+    
+    write_rds(results, paste0(index_file_path, index_name, "/", "STL_Decomposition","/", swinfolder,"/", "stl_", climate_zone, "_", i, ".rds"))#changed output file address manually
+    message ("rds written out")
+  }
 }
 
-Sys.time(); stl(rds_list); Sys.time()
+Sys.time(); stl_function("NDVI", "meanfill_central_dfs", 7, "swindow_7", "central"); Sys.time()
+Sys.time(); stl_function("NDVI", "meanfill_eastern_dfs", 7, "swindow_7", "eastern"); Sys.time()
+Sys.time(); stl_function("NDVI", "meanfill_southern_dfs", 7, "swindow_7", "southern"); Sys.time()
+
+Sys.time(); stl_function("EVI", "meanfill_central_dfs", 7, "swindow_7", "central"); Sys.time()
+Sys.time(); stl_function("EVI", "meanfill_eastern_dfs", 7, "swindow_7", "eastern"); Sys.time()
+Sys.time(); stl_function("EVI", "meanfill_southern_dfs", 7, "swindow_7", "southern"); Sys.time()
+
+Sys.time(); stl_function("NDVI", "meanfill_central_dfs", "periodic", "swindow_periodic", "central"); Sys.time()
+Sys.time(); stl_function("NDVI", "meanfill_eastern_dfs", "periodic", "swindow_periodic", "eastern"); Sys.time()
+Sys.time(); stl_function("NDVI", "meanfill_southern_dfs", "periodic", "swindow_periodic", "southern"); Sys.time()
+
+Sys.time(); stl_function("EVI", "meanfill_central_dfs", "periodic", "swindow_periodic", "central"); Sys.time()
+Sys.time(); stl_function("EVI", "meanfill_eastern_dfs", "periodic", "swindow_periodic", "eastern"); Sys.time()
+Sys.time(); stl_function("EVI", "meanfill_southern_dfs", "periodic", "swindow_periodic", "southern"); Sys.time()
+
