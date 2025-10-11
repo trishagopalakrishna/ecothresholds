ce10e23c856f35ea1e4d470a07ae58bdbb475c1a
Trisha Gopalakrishna <trishagopalakrishna@gmail.com>
2025-10-11 23:23:51 +0530

    New script for complete model results in terms of maps

---
+++ Scripts/Final_Scripts/figures/Fig3_greening_browning_maps.R
@@ -0,0 +1,66 @@
+library(tidyverse)
+library(here)
+library(ggplot2)
+library(ggpubr)
+library(sf)
+library(terra)
+library(tmap)
+library(tmaptools)
+library(RColorBrewer)
+library(viridis)
+library(mgcv)
+library(boot)
+library(scales)
+
+#1.Input needed data and models
+analyses_df_noNA_nooutliers <- read_rds(here("Scripts", "Final_Scripts", "analyses", "analyses_df_noNA_nooutliers.rdata"))
+
+greening_gam <- read_rds(here("Scripts", "Final_Scripts", "analyses", "onemodel_inc.rds"))
+browning_gam <- read_rds(here("Scripts", "Final_Scripts", "analyses" ,"onemodel_dec.rds"))
+
+
+#2. Greening and browning rasters created from data
+prediction_raster <- function(gam_model){
+  pred <- mgcv::predict.bam(gam_model, newdata = analyses_df_noNA_nooutliers, type="terms") 
+  pred <- as.data.frame(pred)
+  
+  pred_without_xy <- pred %>% dplyr::select(-c("s(x,y)"))
+  
+  x_effect <- rowSums(pred_without_xy)
+  x_effect_inv <- inv.logit(x_effect + gam_model$coefficients[1])
+  x_effect_inv <- as.data.frame(x_effect_inv)
+  x_effect_inv <- x_effect_inv %>% bind_cols(analyses_df_noNA_nooutliers %>% 
+                                         dplyr::select(c(x, y)))
+  names(x_effect_inv)[1]<- "main_effect"
+  x_effect_inv <- x_effect_inv %>% dplyr::select(x,y, main_effect)
+  x_rast <- rast(as.data.frame(x_effect_inv %>% drop_na()), type="xyz")
+  
+  x_rast
+}
+
+Sys.time(); greening_rast <- prediction_raster(greening_gam); Sys.time()
+Sys.time(); browning_rast <- prediction_raster(browning_gam); Sys.time()
+
+#2  Maps of greening and browning 
+d_trans<- st_read(here ("Data", "Cattelanetal_Clustering", "cerrado_climate_zones.shp"))
+d_trans<- st_transform(d_trans, crs = 4326)
+d_trans<- d_trans %>% mutate(area_km= terra::expanse(vect(d_trans), unit="km"))
+cerrado<- d_trans %>% st_union()
+
+aggregate_mapping_function <- function (partialeffect_raster, greens_or_brown_values){
+  x_map<- tm_shape(cerrado)+ tm_borders()+
+    tm_polygons(fill.scale = tm_scale_categorical(n=1, values = "grey"))+
+    tm_shape (partialeffect_raster) + 
+    tm_raster(col.scale = tm_scale_continuous(values = greens_or_brown_values),
+              col.legend = tm_legend(show= TRUE, title = "Probability", reverse = T, frame.color = NA)) + 
+    tm_layout(frame = FALSE)
+  x_map
+}
+
+Sys.time(); map_greening <- aggregate_mapping_function(greening_rast, "greens"); Sys.time()
+Sys.time(); map_browning <- aggregate_mapping_function(browning_rast, "brown"); Sys.time()
+
+map_arrange <- tmap_arrange(map_greening, map_browning, nrow = 1, ncol =2)
+tmap_save(map_arrange, here("Outputs", "TrendsResults", "Figures", "Fig4", "map_green_brown.png"), 
+          width = 40, height = 40, units = "cm",
+          dpi = 300)
