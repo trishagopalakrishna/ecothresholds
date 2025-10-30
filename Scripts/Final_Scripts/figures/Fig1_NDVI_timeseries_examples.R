c93fbd9332e0b066a702f8bbe6d4c9b12e9d565f
Trisha Gopalakrishna <trishagopalakrishna@gmail.com>
2025-10-30 10:50:46 +0000

    Redid time series line plots to increase font size of axis labels and make x axis labels black color

--- Scripts/Final_Scripts/figures/Fig1_NDVI_timeseries_examples.R
+++ Scripts/Final_Scripts/figures/Fig1_NDVI_timeseries_examples.R
@@ -9,6 +9,7 @@ library(tmaptools)
 library(RColorBrewer)
 library(viridis)
 
+
 ############################## Fig 1a - Map of NDVI based greening, browning and no trend trajectories + examples of types of trajectories
 #1. Map 
 d_trans<- st_read(here ("Data", "Cattelanetal_Clustering", "cerrado_climate_zones.shp"))
@@ -24,14 +25,15 @@ aggregate_mapping_function <- function (trendresults_raster){
   x_map<- tm_shape(cerrado)+ tm_borders()+
     tm_shape (trendresults_raster) + 
     tm_raster(col.scale = tm_scale_categorical(n=3, values = aggregate_trajectory_palette), 
-              col.legend = tm_legend_hide())
+              col.legend = tm_legend(show = F)) +
+    tm_layout(frame = FALSE)
   x_map
 }
 
 fig1_map <- aggregate_mapping_function(monthly_ts_swin11_results)
 output_file_path <- here("Outputs", "TrendsResults", "aggregate_trajectory_results")
-tmap_save(fig1_map, paste0(output_file_path, "/", "map1km_ndvi_aggtrajectories_monthly_swin11.png"),
-          height = 80, width = 80, units = "cm", dpi=700)
+tmap_save(fig1_map, paste0(output_file_path, "/", "map1km_ndvi_aggtrajectories_monthly_swin11_2.png"),
+          height = 16, width = 16, units = "cm", dpi= 400)
 
 #2. Examples of time series of different types of trajectories
 ts_data <- read.csv(here("Outputs", "TrajectoryPlotting", "ndvi_evi_ts_trajectories.csv"))
@@ -40,46 +42,46 @@ lp_fig<- function (df, title) {
   df %>%  
   ggplot(aes(x = as.factor(time), y= NDVI_trend, group=1))+
   geom_line(linetype="solid")+
-  theme_classic()+
+  theme_classic(base_size = 8)+
     scale_x_discrete(breaks = c("2002_01", "2008_01", "2014_01", "2021_01"), labels = c("2002", "2008", "2014", "2021"))+
   theme (axis.text.y = element_blank(),
          axis.ticks.y = element_blank(),
          axis.title.x = element_blank(),
-         axis.text.x = element_text(angle = 45, hjust=1),
-         plot.title = element_text(hjust = 0.5))+
-  ylab("NDVI decomposed monthly value") + ggtitle(title)+
+         axis.text.x = element_text(angle = 45, hjust=1, colour = "black"),
+         plot.title = element_text(size = 8,hjust = 0.5))+
+  ylab("NDVI decomposed\nmonthly value") + ggtitle(title)+
    scale_alpha(guide = 'none')
 }
 
 lin_inc <- ts_data %>% filter(NDVI_ID==200529) %>% dplyr::select(c(time, NDVI_trend)) %>% mutate(Time2=time) %>%
   separate_wider_delim(Time2, "_", names=c("Year", "Month"))
-fig_lin_inc <- lp_fig(lin_inc, "Linear increase")
+fig_lin_inc <- lp_fig(lin_inc, "Linear greening")
 ggsave(here("Outputs", "TrajectoryPlotting", "maintext_NDVI_lineplots", "lin_inc.png"),
-       fig_lin_inc, dpi = 700, height = 7, width = 7, units = "cm")
+       fig_lin_inc, dpi = 300, height = 4, width = 4, units = "cm")
 
 lin_dec <- ts_data %>% filter(NDVI_ID==3612136) %>% dplyr::select(c(time, NDVI_trend)) %>% mutate(Time2=time) %>%
   separate_wider_delim(Time2, "_", names=c("Year", "Month"))
-fig_lin_dec <- lp_fig(lin_dec, "Linear decrease")
+fig_lin_dec <- lp_fig(lin_dec, "Linear browning")
 ggsave(here("Outputs", "TrajectoryPlotting", "maintext_NDVI_lineplots", "lin_dec.png"),
-       fig_lin_dec, dpi = 700, height = 7, width = 7, units = "cm")
+       fig_lin_dec, dpi = 300, height = 4, width = 4, units = "cm")
 
 step_inc <- ts_data %>% filter(NDVI_ID==1293359) %>% dplyr::select(c(time, NDVI_trend)) %>% mutate(Time2=time) %>%
   separate_wider_delim(Time2, "_", names=c("Year", "Month"))
-fig_step_inc <- lp_fig(step_inc, "Abrupt positive")
+fig_step_inc <- lp_fig(step_inc, "Abrupt greening")
 ggsave(here("Outputs", "TrajectoryPlotting", "maintext_NDVI_lineplots", "step_inc.png"),
-       fig_step_inc, dpi = 700, height = 7, width = 7, units = "cm")
+       fig_step_inc, dpi = 300, height = 4, width = 4, units = "cm")
 
 step_dec <- ts_data %>% filter(NDVI_ID==1280327) %>% dplyr::select(c(time, NDVI_trend)) %>% mutate(Time2=time) %>%
   separate_wider_delim(Time2, "_", names=c("Year", "Month"))
-fig_step_dec <- lp_fig(step_dec, "Abrupt negative")
+fig_step_dec <- lp_fig(step_dec, "Abrupt browning")
 ggsave(here("Outputs", "TrajectoryPlotting", "maintext_NDVI_lineplots", "step_dec.png"),
-       fig_step_dec, dpi = 700, height = 7, width = 7, units = "cm")
+       fig_step_dec, dpi = 300, height = 4, width = 4, units = "cm")
 
 no_trend <- ts_data %>% filter(NDVI_ID==8827783) %>% dplyr::select(c(time, NDVI_trend)) %>% mutate(Time2=time) %>%
   separate_wider_delim(Time2, "_", names=c("Year", "Month"))
 fig_no_trend <- lp_fig(no_trend, "No trend")
 ggsave(here("Outputs", "TrajectoryPlotting", "maintext_NDVI_lineplots", "no_trend.png"),
-       fig_no_trend, dpi = 700, height = 7, width = 7, units = "cm")
+       fig_no_trend, dpi = 300, height = 4, width = 4, units = "cm")
 
 
 ############################## Fig 1b - Characterization of browning, greening and no trends in terms of vegetation formation
@@ -88,25 +90,26 @@ input_filepath <- here("Outputs", "TrendsResults", "Figures")
 byveg<- read_rds(here(input_filepath, "agg_trajectories_vegformations_boxplot.Rdata"))
 
 #1. Vegetation formation
-vegformation_palette<- c("#638b66", "#b66353", "#fbb04e" )
+#vegformation_palette<- c("#638b66", "#b66353", "#fbb04e" )
 
-y_boxplot <- ggplot(byveg, aes(y = value, x = Vegformation)) +
-  geom_boxplot(aes(fill = Vegformation), outlier.shape = NA) + 
-  facet_wrap(.~TrajType) +
+y_boxplot <- ggplot(byveg, aes(y = value, x = TrajType)) +
+  geom_boxplot(aes(fill = TrajType), outlier.shape = NA) + 
+  facet_wrap(.~Vegformation) +
   theme_classic(base_size = 14) +
-  theme(legend.title=element_blank(),
+  theme(legend.position = "none",
         axis.text.x = element_blank(),
         axis.ticks.x = element_blank())+ 
-  scale_fill_manual(values = vegformation_palette) +
-  xlab("Vegetation Formation") + ylab("Proportion of 1km pixel")
-ggsave(here("Outputs","TrendsResults", "Figures", "vegcomposition.png"), y_boxplot,
+  scale_fill_manual(values = aggregate_trajectory_palette) +
+  xlab("Stability Dynamics") + ylab("Proportion of 1km pixel\ncovered by each vegetation formation")
+ggsave(here("Outputs","TrendsResults", "Figures", "vegcomposition2.png"), y_boxplot,
        dpi = 700, height = 15, width = 15, units = "cm")
 
 
 ############################## Fig 1c - Composition of greening, browning, no trend in terms of trajectory types
 trajectorycomposition <- read_rds(here(input_filepath, "trajectorytypes_monthly_swin11.rdata"))
 
-Set1palette_barplot<- c("#994F00",   "#88CCEE" ,  "#332288", "#117733", "lightgrey", "#AA4499", "#882255", "#44AA99", "#CC6677") 
+#Set1palette_barplot<- c("#994F00","#117733", "#bf812d", "#41AB5D", "lightgrey", "#dfc27d", "#f6e8c3", "#74C476","#C7E9C0")
+Set1palette_barplot<- c("#663300","#61A36A", "#bf812d", "#41AB5D" , "#7C7083", "#dfc27d", "#f6e8c3", "#74C476","#C7E9C0")
 
 monthly_ndvi_seperate_plot <- trajectorycomposition %>%
   ggplot(aes(x = factor(agg_traj), y= count/1000, fill = factor(seperate_traj))) + 
@@ -117,7 +120,6 @@ monthly_ndvi_seperate_plot <- trajectorycomposition %>%
   theme(axis.text.x = element_text(angle = 45, vjust=0.7),
         axis.title.x = element_blank(),
         legend.title=element_blank(),
-        axis.ticks.x = element_blank()) + ylab ("Area (*1000 sqkm)")
-ggsave(here("Outputs","TrendsResults", "Figures", "disaggregate_traj.png"), monthly_ndvi_seperate_plot,
+        axis.ticks.x = element_blank()) + ylab ("Proportional area (*1000 sqkm)")
+ggsave(here("Outputs","TrendsResults", "Figures", "disaggregate_traj2.png"), monthly_ndvi_seperate_plot,
        dpi = 700, height = 15, width = 15, units = "cm")
-
